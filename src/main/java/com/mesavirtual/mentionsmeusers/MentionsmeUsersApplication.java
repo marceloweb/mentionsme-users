1a4c3363e1ceb7bc6772b2f79717c9df4df6abb7
Marcelo Oliveira <marcelo.oliveira@gft.com>
2022-03-09 11:16:01 -0300

    initial

---
+++ src/main/java/com/mesavirtual/mentionsmeusers/MentionsmeUsersApplication.java
@@ -0,0 +1,13 @@
+package com.mesavirtual.mentionsmeusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentionsmeUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentionsmeUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesavirtual/mentionsmeusers/MentionsmeUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesavirtual.mentionsmeusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentionsmeUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
